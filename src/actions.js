5b7024b00b4600afa0359577000bc9726629f947
fullcode100 <snowleopard722@outlook.com>
2018-07-27 09:45:50 -0400

    adds switch/case statement to reducers

--- src/actions.js
+++ src/actions.js
@@ -1,8 +1,8 @@
 // Step 2: List out all the ways
 // we could change state
 
-const INCREMENT = 'INCREMENT';
-const DECREMENT = 'DECREMENT';
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
 
 // const actions = {
 //   INCREMENT: 'INCREMENT',
--- src/reducers.js
+++ src/reducers.js
@@ -1,4 +1,30 @@
+import {
+  INCREMENT,
+  DECREMENT
+} from './actions';
+
 // Step 1: Describe the ideal/typical state.
 // {
 //   count: 0
 // }
+
+const initialState = { count: 0 };
+
+// Step 3: Write a reducer that accepts
+// the current state and an action
+const counter = (state=initialState, action) => {
+  if (!action) {
+    return state;
+  }
+
+  switch(action.type) {
+    case INCREMENT:
+      // add 1 to the count
+      break;
+    case DECREMENT:
+      // subtract 1 from the count
+      break;
+    default:
+      return state;
+  }
+}
